e4fed5c428911a89989c657dd9800bc6e2603fe1
Merril Mathew <merrilmathew@Merrils-MacBook-Pro.local>
2018-04-30 12:18:12 +0100

    Vagrant windows-vm

---
+++ scripts/provision.ps1
@@ -0,0 +1,2 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -channel current -project chefdk
